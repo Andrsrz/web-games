a79c4e011adba0a160b45c150e657a5b01160248
Andres Ruiz <andrsruiz@gmail.com>
2020-05-30 13:57:22 -0500

    Add Square

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
+var WIDTH = 640;
+var HEIGHT = 480;
+
 function pageLoaded(){
 	console.log("Hello, world!");
-	windowConfig();
+	let context = windowConfig();
+	draw(context);
 }
 
 function windowConfig(){
 	/* Our canvas */
 	let window = document.getElementById("window");
-	window.setAttribute("width", "640");
-	window.setAttribute("height", "480");
-	window.setAttribute("style", "border: #000 1px solid;");
+	window.setAttribute("width", WIDTH);
+	window.setAttribute("height", HEIGHT);
+	window.setAttribute("style", "border: #000 2px solid;");
+	/* Our 2D context for our canvas */
+	let context = window.getContext("2d");
+	return context;
+}
+
+function draw(canvasContext){
+	/* player */
+	canvasContext.fillRect(10, 10, 50, 50);
 }
