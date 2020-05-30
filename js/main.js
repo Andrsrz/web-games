9fcaa08f9c42214116a244b6d6ae9e9fe143fe91
Andres Ruiz <andrsruiz@gmail.com>
2020-05-30 12:17:51 -0500

    Init Canvas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 function pageLoaded(){
 	console.log("Hello, world!");
+	windowConfig();
+}
+
+function windowConfig(){
+	/* Our canvas */
+	let window = document.getElementById("window");
+	window.setAttribute("width", "640");
+	window.setAttribute("height", "480");
+	window.setAttribute("style", "border: #000 1px solid;");
 }
